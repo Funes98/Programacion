47c3a258dd6e33db5004f03ffe5acea939fd253b
Jesús Pérez Funes <99337378+Funes98@users.noreply.github.com>
2024-02-20 13:14:01 +0100

    Ejercicio3.java

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -3,10 +3,26 @@ package B0;
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		float n1=300;
-		float n2=45;
+		Scanner sc=new Scanner(System.in);
 		
-		System.out.println((n1+n2)/2);
+		Random random=new Random();
+		
+		int numeroSecreto=random.nextInt(100)+1;
+		
+		int num;
+		do {
+			System.out.println("Dime un numero: ");
+			num=sc.nextInt();
+			
+			if (num>numeroSecreto) {
+				System.out.println("El numero que has introducido es mayor");
+			}else if (num==numeroSecreto){
+				System.out.println("Enorabuena, has acertado");
+			}else {
+				System.out.println("El numero que has introducido es menor");
+			}
+			
+		}while(num!=numeroSecreto);
 
 	}
 
